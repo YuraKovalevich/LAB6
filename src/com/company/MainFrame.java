da1850abde3eec75140b3eff2732fe20ea828e17
Yury Kovalevich <kovalevichyura@gmail.com>
2022-03-27 22:10:09 +0300

    add files

--- src/com/company/MainFrame.java
+++ src/com/company/MainFrame.java
@@ -1,2 +1,83 @@
-package com.company;public class MainFrame {
+package com.company;
+
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JMenuItem pauseMenuItem;
+    private JMenuItem pauseTwoMenuItem;
+    private JMenuItem resumeMenuItem;
+    // Поле, по которому прыгают мячи
+    private Field field = new Field();
+    public MainFrame() {
+        super("Программирование и синхронизация потоков");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2, (kit.getScreenSize().height - HEIGHT)/2);
+        setExtendedState(MAXIMIZED_BOTH);
+// Создать меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+        JMenu ballMenu = new JMenu("Мячи");
+        Action addBallAction = new AbstractAction("Добавить мяч") {
+            public void actionPerformed(ActionEvent event) {
+                field.addBall();
+                if (!pauseMenuItem.isEnabled() && !resumeMenuItem.isEnabled()) {
+                    pauseMenuItem.setEnabled(true);
+                    pauseTwoMenuItem.setEnabled(true);
+                }
+            }
+        };
+        menuBar.add(ballMenu);
+        ballMenu.add(addBallAction);
+        JMenu controlMenu = new JMenu("Управление");
+        menuBar.add(controlMenu);
+        Action pauseBigAction = new AbstractAction("Остановить шары с вектором скорости в 1 четверти") {
+            public void actionPerformed(ActionEvent event) {
+                field.pauseBig();
+                pauseMenuItem.setEnabled(false);
+                pauseTwoMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseTwoMenuItem = controlMenu.add(pauseBigAction);
+        pauseTwoMenuItem.setEnabled(false);
+
+        Action pauseAction = new AbstractAction("Приостановить движение"){
+            public void actionPerformed(ActionEvent event) {
+                field.pause();
+                pauseMenuItem.setEnabled(false);
+                pauseTwoMenuItem.setEnabled(false);
+                resumeMenuItem.setEnabled(true);
+            }
+        };
+        pauseMenuItem = controlMenu.add(pauseAction);
+        pauseMenuItem.setEnabled(false);
+
+        Action resumeAction = new AbstractAction("Возобновить движение") {
+            public void actionPerformed(ActionEvent event) {
+                field.resume();
+                pauseMenuItem.setEnabled(true);
+                pauseTwoMenuItem.setEnabled(true);
+                resumeMenuItem.setEnabled(false);
+            }
+        };
+        resumeMenuItem = controlMenu.add(resumeAction);
+        resumeMenuItem.setEnabled(false);
+// Добавить в центр граничной компоновки поле Field
+        getContentPane().add(field, BorderLayout.CENTER);
+    }
+
+
 }
